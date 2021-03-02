88899a0e419f7e108ca9b36ab235c5e3375a9df1
homeuser_cookie <2497430769@qq.com>
2021-03-02 21:31:44 +0800

    更新webpack环境为4，业务代码放置src中

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -4,12 +4,12 @@ module.exports = {
   mode: 'production',
   // 打包那个文件
   entry: {
-    main: './index.js',
+    main: './src/index.js',
   },
   output: {
     // 打包完成生成的名字
     filename: 'bundle.js',
     // 打包生成的文件放在那个文件夹bundle下
-    path: path.resolve(__dirname, 'bundle')
+    path: path.resolve(__dirname, 'dist')
   }
 }
