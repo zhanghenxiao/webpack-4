6fda8acde67dbfbcf57583c483f46632e9307444
homeuser_cookie <2497430769@qq.com>
2021-03-03 11:32:21 +0800

    importLoaders: 2 ,设置互相引用的css文件也需要走下面的二个loader

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,9 +1,5 @@
-// const nav = require('./nav.jpg')
-// console.log(nav); // c7ef824d7c45efa8657b62aae9352e80.jpg 同dist下面的
-
 import nav from './nav.jpg'
-// import './index.css'
-import './nav.scss';
+import './index.scss';
 
 // 创建img标签
 let img = new Image()
